2f81eb9dd5d5978b9132d0e816859aeae68f0313
KOSTEC <3344200+kostec-stat@users.noreply.github.com>
2025-04-25 04:16:13 +0900

    Add files via upload

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "12239b817263fb17656d34b47e2aea20
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "9cc06d036eecd9a7692c92c01959771e",
+"flutter_bootstrap.js": "2ae457d6419e45aa26e16ab77509600a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
